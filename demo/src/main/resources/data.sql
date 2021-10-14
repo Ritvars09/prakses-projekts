030d007bf38ebf078c73f56131a5826e27d6152b
Ritvars <richissh123@gmail.com>
2021-10-14 15:27:14 +0300

    update

--- demo/src/main/resources/data.sql
+++ demo/src/main/resources/data.sql
@@ -1,13 +1,13 @@
-DROP TABLE IF EXISTS billionaires;
+DROP TABLE IF EXISTS Klienti;
 
 CREATE TABLE billionaires (
   id INT AUTO_INCREMENT  PRIMARY KEY,
-  first_name VARCHAR(250) NOT NULL,
-  last_name VARCHAR(250) NOT NULL,
-  career VARCHAR(250) DEFAULT NULL
+  vards VARCHAR(250) NOT NULL,
+  uzvards VARCHAR(250) NOT NULL,
+  vecums VARCHAR(250) DEFAULT NULL
 );
 
-INSERT INTO billionaires (first_name, last_name, career) VALUES
-  ('Aliko', 'Dangote', 'Billionaire Industrialist'),
-  ('Bill', 'Gates', 'Billionaire Tech Entrepreneur'),
-  ('Folrunsho', 'Alakija', 'Billionaire Oil Magnate');
+INSERT INTO billionaires (vards, uzvards, vecums) VALUES
+  ('Janis','Berzs', 11),
+  ('Gatis', 'Kandis',  33),
+  ('Dana', 'Kalnina', 42);
